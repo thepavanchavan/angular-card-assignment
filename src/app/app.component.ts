3e7fde3d81e0857c835e44922cc43720b9c51b61
thepavanchavan <pavanchavan2060@gmail.com>
2021-03-18 20:00:09 +0530

    Implemeted ellipses and Tooltip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,25 +13,22 @@ export class AppComponent {
       id: 1,
       image:'https://images.unsplash.com/photo-1571660336069-a551b9ff9c45?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTYyfHxmcnVpdHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
       title: 'PINAPPLE',
-      para:' The pineapple is a tropical plant.',
+      para:'Pineapple (Ananas comosus) is an incredibly delicious and healthy tropical fruit. It originated in South America',
       pills:["Color","Test","Smell","Sweet","Size"],
-      remainingPillsCount:3,
     },
     {
       id: 2,
       image:'https://images.unsplash.com/photo-1597045315639-9df6c171fb69?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mzh8fGZydWl0fGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
       title: 'APPLE',
-      para:"Apples are among the world's most popular fruits.",
+      para:"Apple trees are cultivated worldwide and are the most widely grown species in the genus Malus",
       pills:["Season","Color","Test","Smell","Sweet","Size","vitamins"],
-      remainingPillsCount:5
     },
     {
       id: 3,
       image:'https://images.unsplash.com/photo-1589533610925-1cffc309ebaa?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDR8fGZydWl0fGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
       title: 'STRAWBERRY',
-      para:'Strawberries improve heart function',
+      para:'The garden strawberry is a widely grown hybrid species of the genus Fragaria, collectively known as the strawberries',
       pills:["Smell","Sweet","Color","Test","Size","Season"],
-      remainingPillsCount:4
     },
   ];
 
@@ -40,25 +37,22 @@ export class AppComponent {
       id: 1,
       image:'https://images.unsplash.com/photo-1571660336069-a551b9ff9c45?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTYyfHxmcnVpdHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
       title: 'pinapple',
-      para:' this has a rosette of pointed leaves up to 90 cm.',
-      pills:["Vitamins","Taste","Smell","Sweet","Size","Season"],
-      remainingPillsCount:4
+      para:' this has a rosette of pointed leaves up to 90 cm.Pineapple (Ananas comosus) is an incredibly delicious',
+      pills:["Vitamins","Sweet","Size"],
     },
     {
       id: 2,
       image:'https://images.unsplash.com/photo-1597045315639-9df6c171fb69?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mzh8fGZydWl0fGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
       title: 'apple',
-      para:"This fruit is good for health.",
+      para:"This fruit is good for health. An apple is an edible fruit produced by an apple tree",
       pills:["Healthy","Energy-Booster","Test","Smell","Sweet","Size","aroma"],
-      remainingPillsCount:5
     },
     {
       id: 3,
       image:'https://images.unsplash.com/photo-1589533610925-1cffc309ebaa?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDR8fGZydWl0fGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
       title: 'strawberry',
-      para:'The fruit is appreciated for its characteristic aroma',
+      para:'The fruit is appreciated for its characteristic aroma The garden strawberry is a widely grown hybrid species ',
       pills:["Aroma","Juicy","Color","Test","Size"],
-      remainingPillsCount:3
     },
   ];
   changeEvent(){
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -8,18 +8,11 @@ import { Component, Input, OnInit } from '@angular/core';
 export class CardComponent implements OnInit {
 
   @Input() dataSet:any= [];
-  // pills:any=[];
-  // pills = this.dataSet.pills
 
 public pillsCount=0;
 
 constructor() {
-//  for (let i = 2; i < this.dataSet.pills.length(); i++) {
-//    this.pillsCount = this.pillsCount+1;
-//  }
-//  console.log(this.pillsCount);
- }
+}
   ngOnInit(){
-    console.log(this.dataSet)
-  };
+}
 }
