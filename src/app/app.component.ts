3d3e2900d05af12a6ac98562f18e3b18d2c61487
thepavanchavan <pavanchavan2060@gmail.com>
2021-03-19 10:18:14 +0530

    Pills interface management according to the conditions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent {
       image:'https://images.unsplash.com/photo-1571660336069-a551b9ff9c45?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTYyfHxmcnVpdHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
       title: 'PINAPPLE',
       para:'Pineapple (Ananas comosus) is an incredibly delicious and healthy tropical fruit. It originated in South America',
-      pills:["Color","Test","Smell","Sweet","Size"],
+      pills:["Color","Test"],
     },
     {
       id: 2,
@@ -38,21 +38,21 @@ export class AppComponent {
       image:'https://images.unsplash.com/photo-1571660336069-a551b9ff9c45?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTYyfHxmcnVpdHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
       title: 'pinapple',
       para:' this has a rosette of pointed leaves up to 90 cm.Pineapple (Ananas comosus) is an incredibly delicious',
-      pills:["Vitamins","Sweet","Size"],
+      pills:[],
     },
     {
       id: 2,
       image:'https://images.unsplash.com/photo-1597045315639-9df6c171fb69?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mzh8fGZydWl0fGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
       title: 'apple',
       para:"This fruit is good for health. An apple is an edible fruit produced by an apple tree",
-      pills:["Healthy","Energy-Booster","Test","Smell","Sweet","Size","aroma"],
+      pills:["Healthy"],
     },
     {
       id: 3,
       image:'https://images.unsplash.com/photo-1589533610925-1cffc309ebaa?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDR8fGZydWl0fGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
       title: 'strawberry',
       para:'The fruit is appreciated for its characteristic aroma The garden strawberry is a widely grown hybrid species ',
-      pills:["Aroma","Juicy","Color","Test","Size"],
+      pills:["Aroma","Juicy"],
     },
   ];
   changeEvent(){
